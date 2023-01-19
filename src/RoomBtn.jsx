e9dadc150111621028d6d12888da9b18811f0f22
Amrit Nayak <amrit.nayak@autodesk.com>
2023-01-20 01:26:16 +0530

    update

--- src/RoomBtn.jsx
+++ src/RoomBtn.jsx
@@ -7,21 +7,28 @@ export default function RoomBtn(props) {
     <div className='customRoom'>
         <Stack direction="row" spacing={2}>
         <h3 className='room'>{props.room}</h3>
+        <Button className='btn' variant="contained" color="success">
+            <a className='switch' href={'http://192.168.1.10/'+props.apiOn}>ON</a>
+        </Button>
+        <Button className='btn' variant="outlined" color="error">
+            <a className='switch' href={'http://192.168.1.10/'+props.apiOff}>OFF</a>
+        </Button>
 
-        <Button className='btn' onClick={() => {
+        {/* <Button className='btn' onClick={() => {
             fetch('http://192.168.1.10/'+props.apiOn)
             console.log('http://192.168.1.10/'+props.apiOn)
         }} variant="contained" color="success">
             ON
         </Button>
-
+        
         <Button className='btn' onClick={() => {
             fetch('http://192.168.1.10/'+props.apiOff)
             console.log('http://192.168.1.10/'+props.apiOff)
         }} variant="outlined" color="error">
             OFF
         </Button>
-        
+         */}
+
         </Stack>
     </div>
   );
