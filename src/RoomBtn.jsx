15ec0be98a91b1ee56bb9c3254a67dd3a0550000
Amrit Nayak <amrit.nayak@autodesk.com>
2023-01-20 10:22:57 +0530

    update4

--- src/RoomBtn.jsx
+++ src/RoomBtn.jsx
@@ -8,15 +8,15 @@ export default function RoomBtn(props) {
         <Stack direction="row" spacing={2}>
         <h3 className='room'>{props.room}</h3>
         
-        <a className='switch' target="_blank" href={'http://192.168.1.10/'+props.apiOn}>
+        <a className='switch' target="_blank" href={'http://192.168.100.197/'+props.apiOn}>
 
-            <Button className='btn' variant="contained" color="success">
+            <Button className='btn' onClick={handleClose} variant="contained" color="success">
                 ON
             </Button>
             
         </a>
         
-        <a className='switch' target="_blank" href={'http://192.168.1.10/'+props.apiOff}>
+        <a className='switch' target="_blank" href={'http://192.168.100.197/'+props.apiOff}>
             <Button className='btn' variant="outlined" color="error">
                 OFF
             </Button>
